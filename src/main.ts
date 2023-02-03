a2b16a8a9176b661d4de20951ea7f122ca6b0c62
juanpablodelgreco <juanpablo.delgreco@hotmail.com>
2023-02-03 00:38:26 -0300

    Add logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getEnvironmentVariable } from './utils/ConfigHelper';
 import { HttpExceptionFilter } from './utils/HttpExceptionFilter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = process.env.APP_PORT || 3000;
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+  const port = getEnvironmentVariable('APP_PORT') || 3000;
 
   app.setGlobalPrefix('ms-car-rest/api/v1.0');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
