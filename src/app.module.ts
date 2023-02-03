32e6f41c4f47deb407e6dc5cf99a7c0691818604
juanpablodelgreco <juanpablo.delgreco@hotmail.com>
2023-02-02 23:49:19 -0300

    Delete unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CarController } from './controllers/CarController';
 import { ConfigModule } from '@nestjs/config';
 import { Car, CarSchema } from './models/Car';
@@ -23,9 +21,8 @@ import { CarServiceImp } from './services/CarServiceImp';
       },
     ]),
   ],
-  controllers: [AppController, CarController],
+  controllers: [CarController],
   providers: [
-    AppService,
     { provide: CAR_REPOSITORY, useClass: CarRepositoryImp },
     { provide: CAR_SERVICE, useClass: CarServiceImp },
   ],
