e290c9eba1eb748464a2e669787db5d66ea61dad
Juan Pablo <juanpablo.delgreco.jp@gmail.com>
2023-02-05 14:46:13 -0300

    Modify HttpExceptionFilter

--- src/utils/HttpExceptionFilter.ts
+++ src/utils/HttpExceptionFilter.ts
@@ -5,19 +5,22 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { ErrorResponse } from 'src/interfaces/errors/ErrorResponse';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    const response: Response = ctx.getResponse<Response>();
+    const request: Request = ctx.getRequest<Request>();
     const status = exception.getStatus();
-    const message = exception.message;
+    let errors: string[] = (exception.getResponse() as ErrorResponse)?.message;
+
+    if (!errors) errors = [exception.getResponse().toString()];
 
     response.status(status).json({
       statusCode: status,
-      message,
+      errors,
       path: request.url,
       timestamp: new Date().toISOString(),
     });
